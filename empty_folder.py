8e715d1bd127ed3c5c6eddf1510a7d494a21eff9
Sam Hecht <samjhecht@gmail.com>
2012-10-23 20:06:01 -0700

    changed filepath

--- empty_folder.py
+++ empty_folder.py
@@ -1,5 +1,5 @@
 import os
-folder = '/home/ubuntu/repo/flatfiles'
+folder = '/home/ubuntu/github_files/'
 for each_file in os.listdir(folder):
 	file_path = os.path.join(folder, each_file)
 	if os.path.isfile(file_path):
